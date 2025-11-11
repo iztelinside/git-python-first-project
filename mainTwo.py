9c927e59f7080ff499dfa7d6e0a3f4b7ba864819
Daniyar Izteleuov <d.iztel@gmail.com>
2025-11-11 20:45:15 +0500

    third change

--- mainTwo.py
+++ mainTwo.py
@@ -1,7 +1,17 @@
 numberOne = int(input("Enter first number: "))
 numberTwo = int(input("Enter second number: "))
 
-if (numberOne >= numberTwo):
+# if (numberOne >= numberTwo):
+#     print(numberOne + numberTwo)
+# else:
+#     print(numberTwo - numberOne)
+
+
+if numberOne >=100 and numberTwo<=500:
     print(numberOne + numberTwo)
+elif numberOne <=0 and numberTwo>=1000:
+    print(numberOne - numberTwo)
+elif numberOne <=1000 and numberTwo>=0:
+    print(numberOne / numberTwo)
 else:
-    print(numberTwo - numberOne)
+    print(numberOne * numberTwo)
