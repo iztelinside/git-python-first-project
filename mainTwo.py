f793ac2444a888853b9e879deec87fae66272442
Daniyar Izteleuov <d.iztel@gmail.com>
2025-11-11 20:21:11 +0500

    second commit

---
+++ mainTwo.py
@@ -0,0 +1,7 @@
+numberOne = int(input("Enter first number: "))
+numberTwo = int(input("Enter second number: "))
+
+if (numberOne >= numberTwo):
+    print(numberOne + numberTwo)
+else:
+    print(numberTwo - numberOne)
