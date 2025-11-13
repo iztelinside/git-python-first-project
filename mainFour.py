329f01d03c6bcb93bdb78f5b10f702ae3b79c282
Daniyar Izteleuov <d.iztel@gmail.com>
2025-11-13 19:01:12 +0500

    Creating a class and object/Constructors, overriding methods

--- mainFour.py
+++ mainFour.py
@@ -1,4 +1,35 @@
+class Car:
+    name =  None
+    price = None
+    color = None
+    def __init__(self,name,price,color):
+        self.set_data(name,price,color)
+        self.get_data()
+    def set_data(self, name = None, price = None, color = None):
+        self.name = name
+        self.price = price
+        self.color = color
 
+    def get_data(self, name=None, price=None, color=None):
+        self.name = name
+        self.price = price
+        self.color = color
+carOne = Car("one",1,"red")
+carTwo = Car("two",2,"blue")
+print(carOne)
+print(carTwo)
+# carOne.set_data("BWM", 90000, "red")
+# carTwo = Car("Audi", 90000, "red")
+# print(carTwo.get_data())
+# carOne.name = "BMW"
+# carOne.price = 50000
+# carOne.color = "red"
+# carTwo = Car()
+# carTwo.name = "Audi"
+# carTwo.price = 70000
+# carTwo.color = "blue"
+carOne.set_data
+carTwo.get_data()
 
 # from mymodule import addNumbers as addN
 #
